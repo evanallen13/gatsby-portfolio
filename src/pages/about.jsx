b250bb106269e578a4a09f875cefe14434cffc27
Evan Allen <evanallen13@gmail.com>
2019-09-04 13:27:25 -0700

    added to about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -13,11 +13,32 @@ const About = () => {
                 page2 = {['/projects','Projects']}
             ></Nav>
             <div className="about">
-                <p style={{marginTop: '10px'}}>I am a full stack developer living in the Los Angeles area.
-                    Currently I am finishing up my last semester at CSULB for my Computer Information Systems degree.
+                <p style={{marginTop: '10px'}}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                Aldus PageMaker including versions of Lorem Ipsum
                 </p>
-                <p>
-                    I am lookin for postions
+                <p style={{marginTop: '10px'}}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                Aldus PageMaker including versions of Lorem Ipsum
+                </p>
+                <p style={{marginTop: '10px', paddingBottom:'50px'}}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                Aldus PageMaker including versions of Lorem Ipsum
                 </p>
             </div>
         </div>
