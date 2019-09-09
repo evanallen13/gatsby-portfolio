b5b5432115a69d6060754a801c482d5398b7627a
Evan Allen <evanallen13@gmail.com>
2019-09-09 15:11:59 -0700

    add domain

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -21,13 +21,6 @@ const About = () => {
                 I am looking for employment as a fullstack developer
                 </p>
                 <p style={{marginTop: '10px',marginBottom: '0', paddingBottom:'50px'}}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                It has survived not only five centuries, but also the leap into electronic typesetting, 
-                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-                Aldus PageMaker including versions of Lorem Ipsum
                 </p>
             </div>
         </div>
