2009841eaaded95450d154bdd7ed42a2789d4137
Evan Allen <evanallen13@gmail.com>
2019-09-28 16:27:19 -0700

    update about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import '../styles/about.css'
 import Nav from '../components/nav/nav'
+import head from '../../static/me/head.png'
+import army from '../../static/me/army.jpg'
 
 const About = () => {
     return (
         <div className='About'>
             <h4>About Me</h4>
+            <img src={head} alt='headshot' style={{width : '80px',display:'inline',borderRadius:"50%"}}></img>
             <Nav
-                btnColor = {'rgb(45, 93, 124)'}
-                color = {'#f3f0e2'}
+                color = {'rgb(45, 93, 124)'}
+                btnColor = {'#f3f0e2'}
                 page1 = {['/','Home']}
                 page2 = {['/projects','Projects']}
             ></Nav>
-            <div className="about">
-                <p style={{marginTop: '10px'}}>
-                I am a front end developer living in the Los Angeles area. I attending my 
-                last semester of my Computer Information Systems degree at CSULB. 
-                </p>
-                <p style={{marginTop: '10px'}}>
-                I am looking for employment as a fullstack developer
-                </p>
-                <p style={{marginTop: '10px',marginBottom: '0', paddingBottom:'50px'}}>
-                </p>
-            </div>
+               <div className="about" >
+                    <h5>Who am I?</h5>
+                    <p>
+                    I am a front end developer living in the Los Angeles area. I attending my 
+                    last semester of my Computer Information Systems degree at CSULB. 
+                    </p>
+                    <p>
+                    I am looking for employment as a fullstack developer
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum dolor iusto repellendus omnis unde soluta reiciendis, porro, dolore id magnam tempora quae officiis nam eveniet delectus perferendis cum in sint.
+                    </p>
+                </div> 
+                <img className="image"src={army} alt='me'></img>
         </div>
     )
 }
